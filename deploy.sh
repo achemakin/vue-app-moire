b80457fe06ebcb7532539a841d24b2b48ebc44a3
achemakin <kotor79@gmail.com>
2021-06-28 21:24:44 +0300

    add deploy.sh and vue.config.js

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# остановить публикацию при ошибках
+set -e
+
+# сборка приложения
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# инициализация репозитория и загрузка кода в GitHub
+git init
+git add .
+git commit -m 'deploy'
+
+git push -f https://github.com/achemakin/vue-app-moire.git master:gh-pages
+
+cd -
